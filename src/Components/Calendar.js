473851ca53fe7de227a5335ebce3c68e855dfb40
DannyCoop <cooperdaniel5141@gmail.com>
2020-09-10 03:48:24 -0400

    the calender work and persist now

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,78 +1,67 @@
-import React from 'react'
-import FullCalendar, { formatDate } from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
+import React, { useState, useEffect } from 'react';
+import FullCalendar from '@fullcalendar/react'
 import interactionPlugin from '@fullcalendar/interaction'
-import { INITIAL_EVENTS, createEventId } from './event-utils'
+import dayGridPlugin from '@fullcalendar/daygrid';
+import {connect, useSelector} from 'react-redux'
 
-export default class Calendar extends React.Component {
+const Calendar = (props) => {
+    const [events, setEvents] = useState([])
+    const currentUser = useSelector(state => state.users.currentUser)
 
-    state = {
-        currentEvents: []
-    }
+    useEffect(() => {fetch("http://localhost:3000/api/v1/calendar_events")
+    .then(res => res.json())
+    .then(data => setEvents(data))},[])
 
-    render() {
-        return (
-            <div className="calendar-container">
-                <FullCalendar
-                    plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
-                    dateClick={this.handleDateClick}
-                    headerToolbar={{
-                        left: 'prev,next today',
-                        center: 'title',
-                        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                    }}
-                    initialView='dayGridMonth'
-                    editable={true}
-                    selectable={true}
-                    selectMirror={true}
-                    dayMaxEvents={true}
-                    initialEvents={INITIAL_EVENTS}
-                    select={this.handleDateSelect}
-                    eventContent={renderEventContent}
-                    eventClick={this.handleEventClick}
-                    eventsSet={this.handleEvents}
-                />
-            </div>
-        )
-    }
+    return(
+        <div className="calendar-container">
+            <FullCalendar 
+                plugins ={[interactionPlugin, dayGridPlugin]}
+                initialView="dayGridMonth"
+                selectable={true}
+                select={(selectInfo) => handleDateSelect(selectInfo, currentUser)}
+                eventClick={handleEventClick}
+                events ={events}
+            />
+        </div>
+    )
 
-//   handleDateClick = (arg) => { // bind with an arrow function
-//     alert(arg.dateStr)
-//     }
-handleDateSelect = (selectInfo) => {
+}
+const handleDateSelect = (selectInfo, currentUser) => {
+    // debugger
     let title = prompt('Please enter a new title for your event')
     let calendarApi = selectInfo.view.calendar
 
     calendarApi.unselect() // clear date selection
 
     if (title) {
-        calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay
+        return fetch("http://localhost:3000/api/v1/calendar_events", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                title: title,
+                date: selectInfo.startStr,
+                end:selectInfo.endStr,
+                user_id: currentUser.id,
+                family_id: currentUser.family_id
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            calendarApi.addEvent(data)
         })
     }
     }
-    handleEventClick = (clickInfo) => {
-        if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-            clickInfo.event.remove()
-        }
-    } 
 
-    handleEvents = (events) => {
-        this.setState({
-            currentEvents: events
-        })
+  const handleEventClick = (clickInfo) => {
+    if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove()
     }
-}
-function renderEventContent(eventInfo) {
-    return (
-    <>
-        <b>{eventInfo.timeText}</b>
-        <i>{eventInfo.event.title}</i>
-    </>
-    )
-}
+  }
+
+
+
+
+
+export default connect()(Calendar)
